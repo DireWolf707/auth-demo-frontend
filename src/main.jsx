f31bca59b41bcafd339ce45c40810c5abfbeb1fe
DireWolf707 <rahulsood707@gmail.com>
2023-03-14 19:54:40 +0530

    added SnackbarProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,23 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import './index.css'
+import "./index.css"
 import { store } from "../store"
 import { Provider as StoreProvider } from "react-redux"
 import { BrowserRouter as Router } from "react-router-dom"
 import { createTheme, ThemeProvider } from "@mui/material"
+import { SnackbarProvider } from "notistack"
 
 const theme = createTheme({})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StoreProvider store={store}>
     <ThemeProvider theme={theme}>
-      <Router>
-        <App />
-      </Router>
+      <SnackbarProvider autoHideDuration={3000} dense={false} maxSnack={4} hideIconVariant={false}>
+        <Router>
+          <App />
+        </Router>
+      </SnackbarProvider>
     </ThemeProvider>
   </StoreProvider>
 )
