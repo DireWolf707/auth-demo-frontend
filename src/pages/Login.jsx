4134923910d641d332f963ce39701b0fef79ebc3
DireWolf707 <rahulsood707@gmail.com>
2023-03-14 17:23:34 +0530

    added backend validation and fixed width in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react"
 import { Stack, Typography, TextField, Button } from "@mui/material"
 import { useLoginMutation } from "../../store"
 import { Link, useNavigate } from "react-router-dom"
+import handleBackendErrors from "../utils/handleBackendErrors"
 
 const Login = () => {
   const navigate = useNavigate()
@@ -17,7 +18,7 @@ const Login = () => {
     login({ email, password })
       .unwrap()
       .then(() => navigate("/"))
-      .catch((err) => console.error(err))
+      .catch(handleBackendErrors())
   }
 
   return (
@@ -26,12 +27,14 @@ const Login = () => {
       component="form"
       flexDirection="column"
       gap={1.2}
-      sx={{ borderRadius: "4px", m: "auto", p: "20px", bgcolor: "white" }}
+      sx={{ borderRadius: "4px", m: "auto", p: "24px 24px 16px 24px", bgcolor: "white", width: "320px" }}
     >
       <Typography variant="h6" fontWeight="bold" color="red" textAlign="center">
         Login
       </Typography>
+
       <TextField inputProps={{ ref: emailRef }} label="Email" variant="outlined" size="small" color="error" required type="email" />
+
       <TextField
         inputProps={{ ref: passwordRef }}
         label="Password"
@@ -41,9 +44,11 @@ const Login = () => {
         required
         type="password"
       />
+
       <Button type="submit" variant="contained" color="error" disabled={isLoading}>
         Submit
       </Button>
+
       <Link to="/forget-password" style={{ textAlign: "end" }}>
         <Typography variant="caption" color="blue">
           Forget Password?
